1467ff2982b6f44b283b32ece1528ef0823bad5e
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-09-13 16:18:25 -0400

    added function to ensure at least 1 of each character type is included in the password. Deleted console logs and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,71 +10,62 @@ var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = [
   '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 // Array that includes each character array
-var allCharactersSelected = []
+var allCharactersSelected = [];
 
-
+// set global variables
 var length = "";
 var lowerCaseConfirm = "";
 var upperCaseConfirm = "";
 var numericConfirm = "";
 var specialConfirm = "";
 
-var result = [];
-// empty array for adding the randomly selected characters to.
+// empty array for setting password characters to.
 var pass = []; 
 
+// Regexp to check for matching criteria in password created
+const regexForDigit = /(\d{1})/;
+const regexForSpecial = /(\W{1})/
+const regexForLowerCase = /([a-z]{1})/;
+const regexForUpperCase = /([A-Z]{1})/;
 
-var getUserInput = function () {
+// function to get desired password length.
+var getLength = function() {
   length = parseInt(prompt("Please enter desired password length. Value must be between 8 and 128."));
-    if (length >= 8 && length <=128) {
-      console.log("Chosen password length is " + length + " .");
-      console.log(length);
-    } else if (Number.isNaN(length)) {
-      alert("a numeric value must be entered");
-      return null;
-    }
-    else {
+  if (length >= 8 && length <=128) {
+    console.log("Chosen password length is " + length + " .");
+  } else if (Number.isNaN(length)) {
+      alert("a numeric value must be entered.");
+      getLength();
+  } else {
       alert("Please enter a value between 8 and 128.")
-      return null;
-    }
-  lowerCaseConfirm = confirm("Do you want to include lower case characters?");
-    if (lowerCaseConfirm) {
-      console.log("lower case characters will be used")
-    } else {
-      console.log("lower case characters will not be used.");
-    }
-  upperCaseConfirm = confirm("Do you want to include upper-case characters?");
-    if (upperCaseConfirm) {
-      console.log("Upper case characters will be used.");
-    } else {
-      console.log("upper case characters will not be used.");
-    }
-  numericConfirm = confirm("Do you want to include numeric characters?");
-    if (numericConfirm) {
-      console.log("Numeric characters will be used.");
-    } else {
-      console.log("Numeric characters will not be used.");
-    }
-  specialConfirm = confirm("Do you want to include special characters?");
-    if (specialConfirm) {
-      console.log("Special characters will be used.");
-    } else {
-      console.log("Special characters will not be used.")
-    } 
-  if (!lowerCaseConfirm && !upperCaseConfirm && !numericConfirm && !specialConfirm) {
-    alert("You must use at least one type of character!")
-    getUserInput();
+      getLength();
   }
 };
 
-// user input values are now available globally
-// create a function which uses the user's input to create a random character of all selected options for each location in the length
-// the loop needs to identify what character types are confirmed / true.
-// the loop needs to generate a random character from the confirmed characters arrays, for each character for length number of characters.
+// function to set desired character types
+var getCriteria = function () {
 
-var generatePassword = function() {
-  debugger;
-  getUserInput();
+  // resets selection confirmation if page is not refreshed;
+  lowerCaseConfirm = false;
+  upperCaseConfirm = false;
+  numericConfirm = false;
+  specialConfirm = false;
+
+  // confirmations for characters to be used.
+  lowerCaseConfirm = confirm("Do you want to include lower case characters?");
+  upperCaseConfirm = confirm("Do you want to include upper-case characters?");
+  numericConfirm = confirm("Do you want to include numeric characters?");
+  specialConfirm = confirm("Do you want to include special characters?");
+  
+  // check to ensure at least 1 type of character was selected. 
+  if (!lowerCaseConfirm && !upperCaseConfirm && !numericConfirm && !specialConfirm) {
+    alert("You must use at least one type of character!")
+    getCriteria();
+  }
+
+  // combine selected characters into 1 array.
+  // resets selected characters array if page is not refreshed.
+  allCharactersSelected = [];
   if (lowerCaseConfirm) {
     allCharactersSelected = allCharactersSelected.concat(lowerCasedCharacters);
   }
@@ -87,16 +78,60 @@ var generatePassword = function() {
   if (specialConfirm) {
     allCharactersSelected = allCharactersSelected.concat(specialCharacters)
   }
-  console.log(allCharactersSelected);
-  
+};
+
+// function to generate password string.
+function passwordLoop() {
+  // reset pass array.
+  pass = [];
   for (var i = 0; i < length; i++) {
+    // store the index 
     var result = Math.floor(Math.random()*allCharactersSelected.length);
+    // add character based on selected character types at index of 'result' to new array.
     pass += (allCharactersSelected[result]);
-    console.log(pass);
-  }
+    }
   return pass;
 };
 
+// function to ensure all characters selected to be included are present in generated password.
+function regexCheck() {
+  if (lowerCaseConfirm) {
+    if (!pass.match(regexForLowerCase)){
+      passwordLoop(); 
+      regexCheck();
+    }
+  };
+
+  if (upperCaseConfirm) {
+    if (!pass.match(regexForUpperCase)) {
+      passwordLoop();
+      regexCheck();
+    }
+  }; 
+  
+  if (numericConfirm) {
+    if (!pass.match(regexForDigit)) {
+      passwordLoop();
+      regexCheck();
+    }
+   };
+
+  if (specialConfirm) {
+    if (!pass.match(regexForSpecial)) {
+      passwordLoop();
+      regexCheck();      
+    }
+   };
+};
+
+var generatePassword = function() {
+  debugger;
+  getLength();
+  getCriteria();
+  passwordLoop();
+  regexCheck(pass);
+  return pass;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
